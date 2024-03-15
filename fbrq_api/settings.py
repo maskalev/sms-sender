1e4ef951116d8e2366288d89d458d5ad4b0bb564
Aleksandr Maskalev <ya.maskalev@yandex.ru>
2024-03-15 19:51:41 +0600

    Fix tests

--- fbrq_api/settings.py
+++ fbrq_api/settings.py
@@ -26,7 +26,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, "api", "templates")
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY", "some-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", True)
