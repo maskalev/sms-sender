962ded4af9a87cd8fc1b87a87744506f8f5f2b35
Aleksandr Maskalev <ya.maskalev@yandex.ru>
2024-03-15 20:01:12 +0600

    Fix workflow

--- fbrq_api/settings.py
+++ fbrq_api/settings.py
@@ -26,10 +26,10 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, "api", "templates")
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY", "some-secret-key")
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", True)
+DEBUG = os.getenv("DEBUG", False)
 
 ALLOWED_HOSTS = [
     "*",
